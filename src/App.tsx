194d4bd9457b1ff2aa905723afb598c789869606
Jesusborjas006 <jesusbor006@gmail.com>
2024-06-28 18:27:59 -0500

    Add default data to meals state

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,26 @@ import Logo from "./components/Logo";
 import Meals from "./components/Meals";
 
 function App() {
-  const [meals, setMeals] = useState<MealType[] | []>([]);
+  const [meals, setMeals] = useState<MealType[]>([
+    {
+      id: 1,
+      food: "potato",
+      quantity: 1,
+      calories: "100",
+    },
+    {
+      id: 2,
+      food: "nuggets",
+      quantity: 3,
+      calories: "180",
+    },
+    {
+      id: 3,
+      food: "watermelon",
+      quantity: 1,
+      calories: "50",
+    },
+  ]);
 
   const addNewMeal = (newMeal: MealType) => {
     setMeals([...meals, newMeal]);
