d8365736ca3c8d5cc0edc040f820470a0eb441b1
Jesusborjas006 <jesusbor006@gmail.com>
2024-06-28 14:39:20 -0500

    Create basic form for the meal being tracked

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,18 @@ function App() {
   return (
     <>
       <h1>Track My Bite</h1>
+      <form>
+        <label htmlFor="food">Food: </label>
+        <input id="food" type="text" />
+        <label htmlFor="quantity">Quanity: </label>
+        <select id="quantity">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+        </select>
+        <label htmlFor="calories">Calories: </label>
+        <input id="calories" type="text" />
+      </form>
     </>
   );
 }
