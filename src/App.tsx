68496da49ee1100ca94139537b2f8f889f777d86
Jesusborjas006 <jesusbor006@gmail.com>
2024-06-28 15:10:58 -0500

    Create form to store meal data in state

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,55 @@
+import { useState } from "react";
+
 function App() {
+  const [meals, setMeals] = useState([]);
+  const [meal, setMeal] = useState({
+    food: "",
+    quantity: 1,
+    calories: "",
+  });
+
+  console.log(meal);
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    setMeal({ ...meal, [e.target.name]: e.target.value });
+  };
+
   return (
     <>
       <h1>Track My Bite</h1>
       <form>
-        <label htmlFor="food">Food: </label>
-        <input id="food" type="text" />
-        <label htmlFor="quantity">Quanity: </label>
-        <select id="quantity">
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-        </select>
-        <label htmlFor="calories">Calories: </label>
-        <input id="calories" type="text" />
+        <label>Food:</label>
+        <input
+          type="text"
+          name="food"
+          onChange={handleChange}
+          value={meal.food}
+        />
+        <label htmlFor="quantity">
+          Quanity:
+          <select
+            id="quantity"
+            name="quantity"
+            value={meal.quantity}
+            onChange={handleChange}
+          >
+            <option value={1}>1</option>
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+          </select>
+        </label>
+        <label htmlFor="calories">
+          Calories:
+          <input
+            id="calories"
+            type="number"
+            name="calories"
+            value={meal.calories}
+            onChange={handleChange}
+          />
+        </label>
       </form>
     </>
   );
