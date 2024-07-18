dab8b210cbcbebab1b2597a3d9e06d83b4f3bf12
Jesusborjas006 <jesusbor006@gmail.com>
2024-07-18 12:34:30 -0500

    Add functionality to navigate user to home page when user input is filled

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,33 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
+  const [user, setUser] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleLogIn = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+
+    if (user) {
+      navigate("/home");
+    }
+  };
+
   return (
     <section className="text-center flex flex-col justify-center items-center border h-screen">
       <h2 className="text-4xl font-bold mb-4">What's your name?</h2>
       <form>
         <label>Name: </label>
-        <input className="border border-black " type="text" />
+        <input
+          className="border border-black "
+          type="text"
+          value={user}
+          onChange={(e) => setUser(e.target.value)}
+        />
+        <button className="border ml-2 px-2 border-black" onClick={handleLogIn}>
+          Log In
+        </button>
       </form>
     </section>
   );
